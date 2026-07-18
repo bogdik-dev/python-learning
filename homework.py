9ca4ca764d21dfe5c1cbadec0b90dda610f55cbe
bogdik-dev <bogdankorendaev@gmail.com>
2026-07-18 03:46:15 +0300

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,60 @@
+age = 20
+height = 1.88
+name = "Bogdik"
+about_me = ["strong","creator","^_^"]
+status = True
+print(age)
+print(height)
+print(name)
+print(status)
+print(about_me)
+
+
+#========Пример2========
+score = 66
+price = 222.8
+city = "Belgorod"
+print(score)
+print(price)
+print(city)
+
+
+#========Пример3========
+a = 33
+b = 20
+c = 33
+print(a == b)
+print(a == c)
+print(a > b)
+print(a < 32)
+
+
+#=========Пример4========
+x = 40
+y = 6
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+print(x % y)
+
+
+#=========Домашняя работа=======
+
+
+word1 = "I"
+word2 = "love"
+word3 = "alt girls"
+string = word1 +" "+ word2 + " " + word3
+print(string)
+
+x = 60
+y = 7
+print(x + y)
+word = "six seveeen"
+print(word)
+
+x = 2211
+y = 33
+print (x / y)
